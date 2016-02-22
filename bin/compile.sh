0d798598a5ee8ab1b5fd11221d3ac5e4d3efc8ae
Dan Dean <dandean@simple.com>
2016-02-22 09:30:23 -0800

    Make compilation script more precise

--- bin/compile.sh
+++ bin/compile.sh
@@ -3,9 +3,15 @@
 PROCESS="compile-react-hoc-validation"
 echo "[$PROCESS] Compiling ES6 to ES5..." 1>&2;
 
-for FILE in $( ls src ); do
+# Iterate through all .js files in the src directory:
+for FILE in $( ls src/*.js ); do
   echo "[$PROCESS] $FILE..." 1>&2;
-  ./node_modules/.bin/babel src/$FILE --out-file $FILE
+
+  # Get the .js file's basename:
+  BASENAME=$( echo $FILE | cut -d "/" -f2)
+
+  # Compile the file with babel, saving the result to the root directory:
+  ./node_modules/.bin/babel $FILE --out-file $BASENAME
 done
 
 echo "[$PROCESS] DONE!" 1>&2;
