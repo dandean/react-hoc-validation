7cbe581f0355600c4461d52d9b54f080b8b12326
Dan Dean <dandean@simple.com>
2016-02-22 22:41:22 -0800

    Start building examples
    Example directory build process is basically entirely cribbed from the
    one in react-router:
    
    https://github.com/reactjs/react-router/tree/master/examples
    
    Thanks @mjackson and @ryanflorence!

---
+++ examples/basic/app.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import classnames from 'classnames';
+
+import {
+  FormValidationManager,
+  FormWrapper,
+  InputWrapper
+} from 'react-hoc-validation';
+
+function required(value, callback) {
+  callback(value.trim().length > 0 ? true : 'Username is required');
+}
+
+class App extends Component {
+  state = {
+    // Store validation state so that we can trigger render by updating it when
+    // the manager's validation state changes:
+    validation: {}
+  };
+
+  componentWillMount() {
+    // Create a form validation manager for this component's form:
+    this.manager = new FormValidationManager();
+  }
+
+  handleValidationChange(previousValue, nextValue) {
+    // Handle the validation state change on the input.
+    this.setState({
+      validation: this.manager.state
+    });
+  }
+
+  render() {
+    // Get the validation state for our field:
+    const usernameValidationState = this.manager.getFieldValidationState('username');
+
+    // Build a class name:
+    const usernameMessageClassName = classnames('message', {
+      valid:        usernameValidationState === true,
+      invalid:      usernameValidationState === false,
+      notvalidated: usernameValidationState === null
+    });
+
+    return (
+      <div>
+        <FormWrapper manager={this.manager}>
+          <form>
+            <label htmlFor="username">Username</label>
+            <InputWrapper manager={this.manager} validators={[required]} onValidationChange={this.handleValidationChange.bind(this)}>
+              <input type="text" name="username" id="username" />
+            </InputWrapper>
+            <span className={usernameMessageClassName}>
+              {this.manager.getFieldValidationMessage('username')}
+            </span>
+
+            <br />
+            <input type="submit" value="submit" />
+          </form>
+        </FormWrapper>
+      </div>
+    )
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('example')
+);
