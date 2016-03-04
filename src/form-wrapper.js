920711d0ac2bf8f516c252110b0d125f0aa36feb
Dan Dean <dandean@simple.com>
2016-03-03 23:11:17 -0800

    Add onValidationChange handler to <FormWrapper>
    Closes #12

--- src/form-wrapper.js
+++ src/form-wrapper.js
@@ -1,11 +1,33 @@
 import React, { Component, PropTypes } from 'react';
+import invariant from 'invariant';
 import FormValidationManager from './manager';
 
 export default class FormWrapper extends Component {
   static propTypes = {
     children: PropTypes.element.isRequired,
-    manager: PropTypes.instanceOf(FormValidationManager).isRequired
+    manager: PropTypes.instanceOf(FormValidationManager).isRequired,
+    onValidationChange: PropTypes.func
   };
+
+  componentWillMount() {
+    invariant(
+      this.props.children && this.props.children.type === 'form',
+      'FormWrapper can only wrap <form> elements'
+    );
+
+    this.handleValidationChange = this.handleValidationChange.bind(this);
+    this.props.manager.addListener('change', this.handleValidationChange);
+  }
+
+  componentWillUnmount() {
+    this.props.manager.removeListener('change', this.handleValidationChange);
+  }
+
+  handleValidationChange() {
+    if (this.props.onValidationChange) {
+      this.props.onValidationChange();
+    }
+  }
 
   render() {
     const originalOnSubmit = this.props.children.props.onSubmit;
