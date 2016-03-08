78e070573275fa332effc9898d73dea8e015804b
Dan Dean <dandean@simple.com>
2016-03-07 22:57:33 -0800

    Add docs to validators

--- src/validators.js
+++ src/validators.js
@@ -1,5 +1,12 @@
 import validator from 'validator';
+import invariant from 'invariant';
 
+/**
+ * Create a required value validator
+ *
+ * @param {String} [message] The validation message
+ * @return {Function} The validator function
+ */
 export function createRequired(message='Required') {
   return function required(value, callback) {
     const valid = validator.isNull(value) === false;
@@ -8,7 +15,17 @@ export function createRequired(message='Required') {
   }
 }
 
+/**
+ * Create a min-length value validator
+ *
+ * @param {Number} length The minimum length
+ * @param {String} [message] The validation message
+ * @return {Function} The validator function
+ */
 export function createMinLength(length, message='Too short') {
+  invariant(Number.isFinite(length), 'Argument `length` must be a number');
+  invariant(length >= 0, 'Argument `length` must be at least zero');
+
   return function minLength(value, callback) {
     const valid = validator.isLength(value, { min: length });
     const response = valid ? null : message ;
@@ -16,7 +33,17 @@ export function createMinLength(length, message='Too short') {
   }
 }
 
+/**
+ * Create a max-length value validator
+ *
+ * @param {Number} length The maximum length
+ * @param {String} [message] The validation message
+ * @return {Function} The validator function
+ */
 export function createMaxLength(length, message='Too long') {
+  invariant(Number.isFinite(length), 'Argument `length` must be a number');
+  invariant(length >= 1, 'Argument `length` must be at least one');
+
   return function maxLength(value, callback) {
     const valid = validator.isLength(value, { max: length });
     const response = valid ? null : message ;
@@ -24,14 +51,12 @@ export function createMaxLength(length, message='Too long') {
   }
 }
 
-export function createMinLength(length, message='Too short') {
-  return function minLength(value, callback) {
-    const valid = validator.isLength(value, { min: length });
-    const response = valid ? null : message ;
-    callback(response);
-  }
-}
-
+/**
+ * Create an integer validator
+ *
+ * @param {String} [message] The validation message
+ * @return {Function} The validator function
+ */
 export function createIsInt(message='Not an integer') {
   return function isInt(value, callback) {
     const valid = validator.isInt(value);
@@ -40,6 +65,16 @@ export function createIsInt(message='Not an integer') {
   }
 }
 
+/**
+ * Create an phone number validator
+ *
+ * See validator.js for locale options:
+ * https://github.com/chriso/validator.js
+ *
+ * @param {String} [locale] The locale to enforce
+ * @param {String} [message] The validation message
+ * @return {Function} The validator function
+ */
 export function createIsPhone(locale='en-US', message='Not a phone number') {
   return function isPhone(value, callback) {
     const valid = validator.isMobilePhone(value, locale);
@@ -48,6 +83,16 @@ export function createIsPhone(locale='en-US', message='Not a phone number') {
   }
 }
 
+/**
+ * Create an email validator
+ *
+ * See validator.js for locale options:
+ * https://github.com/chriso/validator.js
+ *
+ * @param {Object} options Configuration options
+ * @param {String} [message] The validation message
+ * @return {Function} The validator function
+ */
 export function createIsEmail(options, message='Invalid email address') {
   return function isEmail(value, callback) {
     const valid = validator.isEmail(value, options);
@@ -56,6 +101,12 @@ export function createIsEmail(options, message='Invalid email address') {
   }
 }
 
+/**
+ * Create a regular expression validator
+ * @param {RegExp} pattern The regular expression
+ * @param {String} [message] The validation message
+ * @return {Function} The validator function
+ */
 export function createMatches(pattern, message='Does not match pattern') {
   return function matches(value, callback) {
     const valid = validator.matches(pattern);
